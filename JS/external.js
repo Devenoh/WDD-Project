534668a1dcc7c5a9e1b4c78a7a58b73b60c2fa70
Devenoh <habbinote@gmail.com>
2023-12-07 16:40:53 +0000

    more work on game

---
+++ JS/external.js
@@ -0,0 +1,40 @@
+//external.js
+//this function should hide and show images but for some reason it doesnt seem to work
+
+function showhide(){
+	var doc = document.getElementById("hidetask");
+	if(doc.style.display ==="none"){ 
+		doc.style.display = "block"; 
+	}else{
+		doc.style.display ="none";
+	}
+}
+
+function showhide2(){
+	var doc = document.getElementById("hidetask2");
+	if(doc.style.display ==="none"){ 
+		doc.style.display = "block"; 
+	}else{
+		doc.style.display ="none";
+	}
+}
+
+function showhide3(){
+	var doc = document.getElementById("hidetask3");
+	if(doc.style.display ==="none"){ 
+		doc.style.display = "block"; 
+	}else{
+		doc.style.display ="none";
+	}
+}
+
+function showhide4(){
+	var doc = document.getElementById("hidetask4");
+	if(doc.style.display ==="none"){ 
+		doc.style.display = "block"; //assign it to be inline
+	}else{
+		doc.style.display ="none";
+	}
+}
+
+
